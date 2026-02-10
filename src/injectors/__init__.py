ed2081b7c27bdd998379f3b8100059e5632bf821
aaron10l <aaronalvarez7394@gmail.com>
2026-02-09 21:03:38 -0500

    remove unit scale injection template

--- src/injectors/__init__.py
+++ src/injectors/__init__.py
@@ -1,5 +1,4 @@
 from .name_swap_injection import inject as inject_name_swap
-from .unit_scale_injection import inject as inject_unit_scale
 from .heteroskedastic_injection import inject as inject_heteroskedastic
 from .bad_rows_injection import inject as inject_bad_rows
 from .simpsons_paradox_injection import inject as inject_simpsons_paradox
@@ -7,7 +6,6 @@ from .changepoint_injection import inject as inject_changepoint
 
 INJECT_FN = {
     "name_swap_injection": inject_name_swap,
-    "unit_scale_injection": inject_unit_scale,
     "heteroskedastic_injection": inject_heteroskedastic,
     "bad_rows_injection": inject_bad_rows,
     "simpsons_paradox_injection": inject_simpsons_paradox,
