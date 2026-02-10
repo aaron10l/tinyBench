54cf6a8e78d94683e2f4735323a9388164122c3e
aaron10l <aaronalvarez7394@gmail.com>
2026-02-09 21:17:55 -0500

    add answer computer scripts

---
+++ src/answer_computers/__init__.py
@@ -0,0 +1,13 @@
+from .fi_leakage_topk import compute_answer as compute_fi_leakage_topk
+from .anomaly_data_quality_filter import compute_answer as compute_anomaly_data_quality_filter
+from .anomaly_riskier_group import compute_answer as compute_anomaly_riskier_group
+from .rca_retrain_point import compute_answer as compute_rca_retrain_point
+from .rca_performance_improve import compute_answer as compute_rca_performance_improve
+
+COMPUTE_FN = {
+    "fi_leakage_topk_v0": compute_fi_leakage_topk,
+    "anomaly_data_quality_filter_v0": compute_anomaly_data_quality_filter,
+    "anomaly_riskier_group_v0": compute_anomaly_riskier_group,
+    "rca_retrain_point_v0": compute_rca_retrain_point,
+    "rca_performance_improve_v0": compute_rca_performance_improve,
+}
---
+++ src/answer_computers/fi_leakage_topk.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+from typing import Any
+import pandas as pd
+
+
+def compute_answer(df: pd.DataFrame, slot_assignments: dict, effects: dict) -> Any:
+    raise NotImplementedError("Answer computation not yet implemented for fi_leakage_topk")
